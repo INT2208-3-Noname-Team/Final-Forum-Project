3dfe7f563434a92f4258ed9772d400fc075771fc
Le Ngoc Phuong <lengocphuong1122@gmail.com>
2016-05-20 12:15:30 +0700

    create comment data base

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160520103124) do
+ActiveRecord::Schema.define(version: 20160520103125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,17 @@ ActiveRecord::Schema.define(version: 20160520103124) do
 
   add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable", using: :btree
   add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type", using: :btree
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.integer  "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["topic_id"], name: "index_comments_on_topic_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "information", force: :cascade do |t|
     t.string   "sex"
@@ -73,6 +84,8 @@ ActiveRecord::Schema.define(version: 20160520103124) do
     t.string   "remember_digest"
   end
 
+  add_foreign_key "comments", "topics"
+  add_foreign_key "comments", "users"
   add_foreign_key "information", "users"
   add_foreign_key "topics", "categories"
   add_foreign_key "topics", "users"
