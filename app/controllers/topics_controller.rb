f176885d9ff265c01fda9a4c0889cefd9e7dab29
dinh van linh <dinhvanlinh96@gmail.com>
2016-05-20 11:54:05 +0700

    create show method

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -26,8 +26,6 @@ class TopicsController < ApplicationController
     @user = User.find_by_id(@topic.user_id)
     @info = @user.information
     @category = Category.find_by_id(@topic.category_id)
-    # @comment = @topic.comments.all
-    # @new_comment = Comment.new
     @user_now = current_user
   end
 
